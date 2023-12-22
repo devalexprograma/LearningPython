f6af0181b82d08dcb728dafed85e55e3e9c0a088
Alexander Atarama <dev.alexatarama@gmail.com>
2023-12-22 09:37:42 -0500

    Primeros pasos

--- intro.py
+++ intro.py
@@ -1,3 +1,21 @@
-print("Hola mundo")
-num = 12
-print(num)
+nombre = "Edward"
+Texto = """Hola me llamo
+edward y estuduio informatica
+soy experto"""
+
+# Ejemplo Prefix y Suffix - Quita los prefijos y sufijos
+
+producto1 = "0001 - Manzana Mordida"
+producto2 = "Manzana Mordida - 0001"
+
+print(producto1.removeprefix("0001 -"))
+print(producto2.removesuffix("- 0001"))
+
+# Indexing - Muestra un caracter de la cadena
+print(producto2[2])
+# Slicing
+telefono = "4-5-3-3-4-5-6-7-7"
+print(producto2[0:4])
+print(telefono[::2])  # Establece salto de dos
+# Slicing Negativo
+print(producto1[-6:-1])
